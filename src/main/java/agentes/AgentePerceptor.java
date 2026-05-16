3d3c01606d82cb348faf581685b858c24785fa81
alejandragongila <alejandra.gongila@alumnos.upm.es>
2026-05-16 21:34:59 +0200

    Agente perceptor de mensajes en discord

---
+++ src/main/java/agentes/AgentePerceptor.java
@@ -0,0 +1,24 @@
+package agentes;
+
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class AgentePerceptor extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        // Ignora los mensajes enviados por otros bots 
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        // Extrae la información básica del mensaje recibido
+        String autor = event.getAuthor().getAsTag();
+        String canal = event.getChannel().getName();
+        String contenido = event.getMessage().getContentRaw();
+
+        // Imprime en consola quién habló, en qué canal y qué escribió
+        System.out.println("[Perceptor] Mensaje recibido en #" + canal
+                + " de " + autor + ": " + contenido);
+    }
+}
