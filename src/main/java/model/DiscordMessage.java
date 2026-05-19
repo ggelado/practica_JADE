c6dd6a37a9d193713b4d04ebb15face3d54d1c7c
Gonzalo Gelado Rodríguez <gonzalo.gelado@alumnos.upm.es>
2026-05-19 17:39:46 +0200

    Agregado enum de categorías mensajes

--- src/main/java/model/DiscordMessage.java
+++ src/main/java/model/DiscordMessage.java
@@ -1,13 +1,40 @@
 package model;
 
-public class DiscordMessage {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
+public class DiscordMessage implements Serializable {
+
+  public enum Detecciones {
+    GUN, BLOOD, VIOLENCE, FIGHT, NAZI //TODO COMPLETAR TIPOS Y CATEGORÍAS
+  }
+  
   private String mensaje;
   private String id_mensaje;
+  private List<Detecciones> findings;
   
   public DiscordMessage(String msg, String id) {
     this.mensaje=msg;
     this.id_mensaje=id;
+    this.findings = new ArrayList<>();
   }
+  
+  public String getMensaje() {
+    return mensaje;
+  }
+  public String getId() {
+    return id_mensaje;
+  }
+  
+  public void agregarDetecciones(Detecciones deteccion) {
+    findings.add(deteccion);
+  }
+  
+  public Collection<Detecciones> getDetecciones() {
+    return new ArrayList<>(findings);
+  }
+  
   
 }
