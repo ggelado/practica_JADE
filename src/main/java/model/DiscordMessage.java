fcf6d27a8ca50f430bd5c0ce600f67f57aeb0b77
Gonzalo Gelado Rodríguez <gonzalo.gelado@alumnos.upm.es>
2026-05-19 17:27:30 +0200

    Create DiscordMessage.java

---
+++ src/main/java/model/DiscordMessage.java
@@ -0,0 +1,13 @@
+package model;
+
+public class DiscordMessage {
+
+  private String mensaje;
+  private String id_mensaje;
+  
+  public DiscordMessage(String msg, String id) {
+    this.mensaje=msg;
+    this.id_mensaje=id;
+  }
+  
+}
