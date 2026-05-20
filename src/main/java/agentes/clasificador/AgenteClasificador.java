2b99ae0fb259c21e9f1e8fd9b2ac2a1279b0f515
Fabior26 <fabiofranciscorosquetec@gmail.com>
2026-05-20 14:39:44 +0200

    Update AgenteClasificador.java

--- src/main/java/agentes/clasificador/AgenteClasificador.java
+++ src/main/java/agentes/clasificador/AgenteClasificador.java
@@ -3,9 +3,15 @@ package agentes.clasificador;
 import jade.core.Agent;
 
 public class AgenteClasificador extends Agent {
+	  private static final String SERVICE_TYPE = "clasificador";
+	  private static final String SERVICE_NAME = "Servicio-Clasificador";
+	  private static final String ONTOLOGY_PATH = "ontologia.rdf";
+	  private static final String NS = "http://www.discord.monitor/ontologia#";
 
     @Override
     protected void setup() {
-        // TODO pendiente de hacer
+    	 System.out.println("[AgenteClasificador] Arrancando: " + getAID().getName());
+    	 registerService();
+    	 
     }
 }
